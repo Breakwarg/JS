af0feb302af3e8f4ee7c95d0a5e3e1a5611c3332
Lucas <lucascorrea012@gmail.com>
2024-12-12 19:51:28 -0300

    tests aula7

---
+++ aula7/tests.js
@@ -0,0 +1,14 @@
+const nome = 'Luiz Otávio ';
+const sobrenome = 'Miranda' ;
+const idade = 30;
+const peso = 84;
+const alturaemM = 1.80;
+let indiceMassaCorporal; 
+let anoNascimento;
+
+indiceMassaCorporal = peso/ (alturaemM*alturaemM);
+anoNascimento = 2019 - idade;
+console.log(anoNascimento);
+console.log(`${nome} ${sobrenome} tem ${idade} anos pesa ${peso} kg`);
+console.log(`tem,${alturaemM},de altura e seu imc é de, ${indiceMassaCorporal}`);
+console.log(`${nome} nasceu em ${anoNascimento}.`);
