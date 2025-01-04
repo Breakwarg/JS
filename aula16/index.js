4085328100015c57a96f400554a5d9800d682f52
Lucas <lucascorrea012@gmail.com>
2025-01-04 10:18:54 -0300

    Aulas js udemy

---
+++ aula16/index.js
@@ -0,0 +1,28 @@
+//            012345678910
+//const nome = 'Luiz Otavio'
+//console.log(nome[10]);
+//                0      1      2
+
+
+let alunos = ['Luiz','Maria','João'];
+alunos = 123;
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+//alunos.unshift('Luiza'); //adiciona no começo
+
+
+//alunos.push('Luiza');
+//alunos.push('Fábio');
+
+// 
+//alunos[0] = 'Eduardo';
+//alunos[3] = 'Luiza';
+
+
+
+
+//console.log(alunos);
+//console.log(alunos[0]);
+//console.log(alunos[1]);
+//console.log(alunos[2]);
